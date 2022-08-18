b6b41696cd186e61f2890e3cdb62bb8ca455893f
marcossmecel <marcos.smecel@gmail.com>
2022-08-18 17:58:10 -0400

    Quase no fim

--- select.php
+++ select.php
@@ -1,26 +1,80 @@
-<?php
+<!doctype html>
+<html lang="pt-br">
 
-//Variáveis para conexão
-$host = 'localhost';
-$usuario_bd = 'root';
-$senha_bd = '';
-$banco = 'curso1_bd';
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-//Conexão
-$conexao = new mysqli($host, $usuario_bd, $senha_bd, $banco);
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-if ($conexao) {
-    echo 'Conectado';
-    // $query = "INSERT INTO pessoas(nome, email, senha) VALUES ('$nome', '$email', '$senha')";
-    
-    // $result = $conexao->query($query);
-    // if ($result) {
-    //     echo 'Dados inseridos com sucesso!';
-    // } else {
-    //     echo 'Falha ao tentar inserir dados';
-    //     echo mysqli_error($conexao);
-    // }
-} else {
-    echo 'Erro ao tentar conexão!';
-    return;
-}
+    <title>Curso PHP</title>
+</head>
+
+<body>
+
+    <div class="container col-md-6">
+
+        <h1>Curso PHP</h1>
+
+        <h3>Tabela de Pessoas Cadastradas</h3>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+
+                //Variáveis para conexão
+                $host = 'localhost';
+                $usuario_bd = 'root';
+                $senha_bd = '';
+                $banco = 'curso1_bd';
+
+                //Conexão
+                $conexao = new mysqli($host, $usuario_bd, $senha_bd, $banco);
+
+                if ($conexao) {
+
+                    //Selecione tudo da tabela pessoas
+                    $query = "SELECT * FROM pessoas";
+
+                    //Resultado
+                    $result = $conexao->query($query);
+
+                    if ($result) {
+                        //Enquanto tiver linha com resultado
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo  '<tr>';
+                            echo '<td>'.$row["id"].'</td>';
+                            echo '<td>'.$row["nome"].'</td>';
+                            echo '<td>'.$row["email"].'</td>';
+                        echo '</tr>';
+                        }
+                    } else {
+                        echo mysqli_error($conexao);
+                    }
+                } else {
+                    echo 'Erro ao tentar conexão!';
+                    return;
+                }
+
+                ?>
+
+
+            </tbody>
+        </table>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
